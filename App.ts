0791358401b515b17c51e5858074b034e9b2a488
tpranoto <{ID}+{username}@users.noreply.github.com>
2024-11-20 18:59:47 -0800

    add allowed method middleware

--- App.ts
+++ App.ts
@@ -34,6 +34,7 @@ class App {
     this.expressApp.use(bodyParser.urlencoded({ extended: false }));
     this.expressApp.use((req, res, next) => {
       res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
       res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
